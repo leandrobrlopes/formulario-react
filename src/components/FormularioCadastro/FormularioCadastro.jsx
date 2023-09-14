3e98b168153af5bb72dcf0efb5be87a85bc38acb
Leandro Lopes <110497514+leandrobrlopes@users.noreply.github.com>
2023-09-14 20:11:34 -0300

    Update FormularioCadastro.jsx

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 
 function FormularioCadastro(){
-
+    const [nome, setNome] = useState("Leandro");
+    
     return(
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+        <form onSubmit={(event) => {event.preventDefault(); console.log(nome);}}>
+            <TextField value={nome} onChange={event => {setNome(event.target.value); if (nome.length >= 3) {setNome(nome.substr(0,3));}}} id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
             <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
             <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
 
