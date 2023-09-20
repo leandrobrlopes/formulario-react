0504cf519717264fb2b46ed5c79546ad48cf35f5
Leandro Lopes <110497514+leandrobrlopes@users.noreply.github.com>
2023-09-20 15:58:54 -0300

    colocando switch

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,32 +1,35 @@
 import React, {useState} from "react";
-import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@mui/material";
 
 
 function FormularioCadastro({aoEnviar, validarCPF}){
-    const [nome, setNome] = useState("Leandro");
-    const [sobrenome, setSobrenome] = useState("");
-    const [cpf, setCpf] = useState("");
-    const [promocoes, setPromocoes] = useState(true);
-    const [novidades, setNovidades] = useState(true);
-    const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
+    const [etapaAtual, setEtapaAtual] = useState(0);
+
+    function formularioAtual(etapa){
+        switch(etapa){
+            case 0:
+                return <DadosUsuario />
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+            case 2:
+                return <DadosEntrega />
+            default:
+                return <Typography>Erro ao selecionar formulário</Typography>
+        }
+    }
+
     return(
-        <form onSubmit={(event) => {event.preventDefault(); aoEnviar({nome, sobrenome, cpf, novidades, promocoes});}}>
-            <TextField value={nome} onChange={event => { setNome(event.target.value); }} id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
-            <TextField value={sobrenome} onChange={event => {setSobrenome(event.target.value); }} id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
-            <TextField value={cpf} onChange={event => {setCpf(event.target.value); }} onBlur={(event) =>{const ehValido = validarCPF(cpf); setErros({cpf:ehValido})}} error={!erros.cpf.valido} helperText={erros.cpf.texto} id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
-
-            <FormControlLabel label="Promoções" control={<Switch checked={promocoes} onChange={(event) =>{
-                setPromocoes(event.target.checked)
-            }} name="promocoes"  />} />
+        <>
+            { formularioAtual(etapaAtual) }
             
-            <FormControlLabel label="Novidades" control={<Switch checked={novidades} onChange={(event) =>{
-                setNovidades(event.target.checked)
-            }} name="novidades"  />} />
-
-            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
-        </form>
+        </>
     );
 
 }
+
+
 
 export default FormularioCadastro;
